d6762c569c0cdb08f9998f2dae66d04f536fe757
Alvaro Fernandez <alvarofq91@gmail.com>
2024-08-08 09:44:03 +0200

    [nats] Nats start and stop subscription implemented. Also implemented data publish.

--- examples/nats_publish.py
+++ examples/nats_publish.py
@@ -1,12 +1,14 @@
 import asyncio
 import nats
-from nats.errors import TimeoutError
+import argparse
 
 
-async def main():
+async def main(subject:str, message:str="Hello"):
+    # Nos conectamos al servidor local de NATS
     nc = await nats.connect("nats://localhost:4222")
 
-    await nc.publish("sensor.stop", b'Hello')
+    # Publicamos el mensaje con el subject dados
+    await nc.publish(subject, message.encode('utf-8'))
 
     print(f"Published")
     await nc.flush()
@@ -15,4 +17,11 @@ async def main():
     
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    parser = argparse.ArgumentParser(description="Enviar mensajes a un servidor NATS")
+    parser.add_argument('--subject', type=str, required=True, help="Subject del mensaje")
+    parser.add_argument('--message', type=str, required=False, help="Mensaje a enviar")
+
+    args = parser.parse_args()
+
+    # Ejecutamos la funcion main
+    asyncio.run(main(subject=args.subject, message=args.message))
--- src/sensor_app/main.py
+++ src/sensor_app/main.py
@@ -1,29 +1,78 @@
 import asyncio
-import nats
+from nats.aio.client import Client as NATS
 from nats.errors import TimeoutError
 
-async def main():
-    nc = await nats.connect("nats://localhost:4222")
+class NatsHandler:
+    """
+    Clase que maneja los mensajes NATS
+    """
+    def __init__(self) -> None:
+        self.sensor_state = False
+        self.nc = NATS()
 
-    async def message_handler(msg):
-        subject = msg.subject
+    async def start(self):
+        """
+        Conecta con el servidor NATS y se suscribe a los subjects de inicio y parada del sensor
+        """
+        # Conectamos con servidor local NATS
+        await self.nc.connect("nats://localhost:4222")
+        # Nos subscribimos a los subjets "sensor.start" y "sensor.stop"
+        await self.nc.subscribe("sensor.start", cb=self.start_sensor)
+        await self.nc.subscribe("sensor.stop", cb=self.stop_sensor)
 
-        # Vemos si el subject es para iniciar la recogida de datos del sensor
-        if subject == "sensor.start":
-            print("Inicio de recogida de datos del sensor")
-        elif subject == "sensor.stop":
-            print("Parada de recogida de datos del sensor")
+    async def start_sensor(self, msg):
+        """ 
+        Callback para iniciar la captura de datos del sensor.
 
-    await nc.subscribe("sensor.start", cb=message_handler)
-    await nc.subscribe("sensor.stop", cb=message_handler)
+        Args:
+            msg (nats.aio.client.Msg): Mensaje subscrito
+        """
+        self.sensor_state = True
+        print("Inicio de recogida de datos del sensor")
 
+    async def stop_sensor(self, msg):
+        """ 
+        Callback para parar la captura de datos del sensor.
+
+        Args:
+            msg (nats.aio.client.Msg): Mensaje subscrito
+        """
+        self.sensor_state = False
+        print("Parada de recogida de datos del sensor")
+
+    async def run(self):
+        """
+        Bucle infinito para realizar la lectura del sensor
+        """
+        while True:
+            if self.sensor_state:
+                # Empezamos a capturar datos de sensores
+                print("Captura de datos sensor")
+                # Publicamos datos
+                await self.nc.publish(
+                    subject="sensor.data", 
+                    payload=b"Test"
+                )
+                # Dejamos n segundos entre captura y captura
+                await asyncio.sleep(10)
+            else:
+                # Dejamos 1 segunda para que no se quede parado en bucle infinito
+                await asyncio.sleep(1)
+        
 
 
 if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(main())
-    #asyncio.run(main())
+    #Creamos instancia de la clase NatsHandler
+    app = NatsHandler()
 
+    # Obtenemos el bucle de eventos de asyncio
+    loop = asyncio.get_event_loop()
+
+    # Ejecutamos las funciones start y run hasta que se completen
+    asyncio.ensure_future(app.start())
+    asyncio.ensure_future(app.run())
+
+    # Ejecutamos el bucle de eventos hasta el infinito
     try:
         loop.run_forever()
     finally:
